e80b43c5dd4b52c3d54a20b827ebd2d276f10c49
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-07-10 18:58:03 +0800

    Added titlelize-namespace.js file.

---
+++ titlelize-namespace.js
@@ -0,0 +1,23 @@
+/*:
+	@module-configuration:
+		{
+			"packageName": "titlelize-namespace",
+			"fileName": "titlelize-namespace.js",
+			"moduleName": "titlelizeNamespace",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/titlelize-namespace.git",
+			"testCase": "titlelize-namespace-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+*/
+var titlelizeNamespace = function titlelizeNamespace( namespace ){
+
+};
+
+( module || { } ).exports = titlelizeNamespace;
